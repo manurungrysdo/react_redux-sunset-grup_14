6fe0a42fcdb54b0b3997e68acb2f5e070b39d193
Rysdo Manurung <rysdomanurung22@gmail.com>
2024-05-13 23:17:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import UserComponent from "./componen/UserComponent";
+import CounterComponent from "./componen/counter_componen";
+
+function App() {
+  return (
+    <>
+      <CounterComponent></CounterComponent>
+      <UserComponent></UserComponent>
+    </>
+  );
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slinces/counterSlice';
+import userReducer from './slinces/userSlice';
+const store = configureStore({
+reducer: {
+counter: counterReducer,
+user: userReducer,
+}
+});
+export default store;
